3016214dca45168af0f9ac0386121117ddf8deca
joaoalvesss <up202108670@up.pt>
2023-12-09 19:51:47 +0000

    Huge refactor needed

--- Project 2 Code/include/download.h
+++ Project 2 Code/include/download.h
@@ -1,37 +1,70 @@
-#ifndef DOWNLOAD_H
-#define DOWNLOAD_H
-
-#include <string.h>
 #include <stdio.h>
-#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
 #include <netdb.h>
-#include <strings.h>
-#include <ctype.h>
+#include <unistd.h>
+#include <string.h>
+#include <regex.h>
+#include <termios.h>
 
-#define SERVER_PORT 21
-#define SERVER_ADDR "192.168.28.96"
-#define MAX_SIZE 500
+#define MAX_LENGTH  500
+#define SERVER_PORT    21
 
-void readResponse(int socketfd, char *responseCode);
+/* Server responses */
+#define SV_READY4AUTH           220
+#define SV_READY4PASS           331
+#define SV_LOGINSUCCESS         230
+#define SV_PASSIVE              227
+#define SV_READY4TRANSFER       150
+#define SV_TRANSFER_COMPLETE    226
+#define SV_GOODBYE              221
 
-struct hostent *getHostInfo(const char host[]);
+/* Parser regular expressions */
+#define AT              "@"
+#define BAR             "/"
+#define HOST_REGEX      "%*[^/]//%[^/]"
+#define HOST_AT_REGEX   "%*[^/]//%*[^@]@%[^/]"
+#define RESOURCE_REGEX  "%*[^/]//%*[^/]/%s"
+#define USER_REGEX      "%*[^/]//%[^:/]"
+#define PASS_REGEX      "%*[^/]//%*[^:]:%[^@\n$]"
+#define RESPCODE_REGEX  "%d"
+#define PASSIVE_REGEX   "%*[^(](%d,%d,%d,%d,%d,%d)%*[^\n$)]"
 
-void getFile(int socket, char* filename);
+/* Default login for case 'ftp://<host>/<url-path>' */
+#define DEFAULT_USER        "anonymous"
+#define DEFAULT_PASSWORD    "password"
 
-void parseArgument(char *argument, char *user, char *pass, char *host, char *path);
 
-int sendCommandInterpretResponse(int socketfd, char cmd[], char commandContent[], char* filename, int socketfdClient); // TODO
+struct URL {
+    char host[MAX_LENGTH];      // 'ftp.up.pt'
+    char resource[MAX_LENGTH];  // 'parrot/misc/canary/warrant-canary-0.txt'
+    char file[MAX_LENGTH];      // 'warrant-canary-0.txt'
+    char user[MAX_LENGTH];      // 'username'
+    char password[MAX_LENGTH];  // 'password'
+    char ip[MAX_LENGTH];        // 193.137.29.15
+};
 
-int getServerPortFromResponse(int socketfd); // TODO
+typedef enum {
+    START,
+    SINGLE,
+    MULTIPLE,
+    END
+} ResponseState;
 
-void parseFilename(char *path, char *filename);
+int parse(char *input, struct URL *url);
 
 int createSocket(char *ip, int port);
 
-#endif /* DOWNLOAD_H */
+int authConn(const int socket, const char *user, const char *pass);
+
+int readResponse(const int socket, char *buffer);
+
+int passiveMode(const int socket, char* ip, int *port);
+
+int requestResource(const int socket, char *resource);
+
+int getResource(const int socketA, const int socketB, char *filename);
+
+int closeConnection(const int socketA, const int socketB);
--- Project 2 Code/src/download.c
+++ Project 2 Code/src/download.c
@@ -1,74 +1,46 @@
 #include "../include/download.h"
 
-/**
- * The struct hostent (host entry) with its terms documented
+void parseURLWithoutUserInfo(char *input, struct URL *url) {
+    sscanf(input, HOST_REGEX, url->host);
+    strcpy(url->user, DEFAULT_USER);
+    strcpy(url->password, DEFAULT_PASSWORD);
+    sscanf(input, RESOURCE_REGEX, url->resource);
+    strcpy(url->file, strrchr(input, '/') + 1);
+}
 
-    struct hostent {
-        char *h_name;    // Official name of the host.
-        char **h_aliases;    // A NULL-terminated array of alternate names for the host.
-        int h_addrtype;    // The type of address being returned; usually AF_INET.
-        int h_length;    // The length of the address in bytes.
-        char **h_addr_list;    // A zero-terminated array of network addresses for the host.
-        // Host addresses are in Network Byte Order.
-    };
+void parseURLWithUserInfo(char *input, struct URL *url) {
+    sscanf(input, HOST_AT_REGEX, url->host);
+    sscanf(input, USER_REGEX, url->user);
+    sscanf(input, PASS_REGEX, url->password);
+    sscanf(input, RESOURCE_REGEX, url->resource);
+    strcpy(url->file, strrchr(input, '/') + 1);
+}
 
-    #define h_addr h_addr_list[0]	The first address in h_addr_list.
-*/
+int parse(char *input, struct URL *url) {
+    regex_t barRegex, atRegex;
+    regcomp(&barRegex, BAR, 0);
+    if (regexec(&barRegex, input, 0, NULL, 0)) return -1;
 
-int main(int argc, char **argv) {
-     int socketfd, socketfdClient = -1;
-     struct sockaddr_in server_addr, server_addr_client;
-     struct hostent *h;
+    regcomp(&atRegex, AT, 0);
+    if (regexec(&atRegex, input, 0, NULL, 0) != 0) {
+        parseURLWithoutUserInfo(input, url);
+    } else {
+        parseURLWithUserInfo(input, url);
+        // Check if the user field is empty and set it to the default user
+        if (strlen(url->user) == 0) {
+            strcpy(url->user, DEFAULT_USER);
+        }
+    }
 
-     char user[80], pass[80], host[80], path[80], filename[80], responseCode[3];
+    struct hostent *h;
+    if (strlen(url->host) == 0) return -1;
+    if ((h = gethostbyname(url->host)) == NULL) {
+        printf("Invalid hostname '%s'\n", url->host);
+        exit(-1);
+    }
+    strcpy(url->ip, inet_ntoa(*((struct in_addr *) h->h_addr)));
 
-     memset(user, 0, 80);
-     memset(pass, 0, 80);
-     memset(host, 0, 80);
-     memset(path, 0, 80);
-
-     parseArgument(argv[1], user, pass, host, path);
-     parseFilename(path, filename);
-
-     printf(" > Username: %s\n > Password: %s\n > Host: %s\n > Path: %s\n > Filename: %s\n", user, pass, host, path, filename);
-
-     h = getHostInfo(host);
-     printf(" - IP Address: %s\n\n", inet_ntoa(*((struct in_addr *)h->h_addr)));
-
-     socketfd = createSocket(inet_ntoa(*((struct in_addr *)h->h_addr)), SERVER_PORT);
-     readResponse(socketfd, responseCode);
-
-     if (responseCode[0] == '2') {
-          printf(" > Connection Established\n");
-     }
-
-     printf(" > Sending Username\n");
-     int res = sendCommandInterpretResponse(socketfd, "user ", user, filename, socketfdClient);
-
-     if (res == 1) {
-          printf(" > Sending Password\n");
-          res = sendCommandInterpretResponse(socketfd, "pass ", pass, filename, socketfdClient);
-     }
-
-     write(socketfd, "pasv\n", 5);
-     int serverPort = getServerPortFromResponse(socketfd);
-
-     socketfdClient = createSocket(inet_ntoa(*((struct in_addr *)h->h_addr)), serverPort);
-     printf("\n > Sending Retr\n");
-
-     int resRetr = sendCommandInterpretResponse(socketfd, "retr ", path, filename, socketfdClient);
-
-     if (resRetr == 0) {
-          close(socketfdClient);
-          close(socketfd);
-          exit(0);
-     } else {
-          printf(" > ERROR in RETR response\n");
-     }
-
-     close(socketfdClient);
-     close(socketfd);
-     exit(1);
+    return !(strlen(url->host) && strlen(url->user) && strlen(url->password) && strlen(url->resource) && strlen(url->file));
 }
 
 int createSocket(char *ip, int port) { // copied from given code
@@ -77,15 +49,14 @@ int createSocket(char *ip, int port) { // copied from given code
 
      bzero((char *) &server_addr, sizeof(server_addr));
      server_addr.sin_family = AF_INET;
-     server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);  /*32 bit Internet address network byte ordered*/
-     server_addr.sin_port = htons(SERVER_PORT);             /*server TCP port must be network byte ordered*/
+     server_addr.sin_addr.s_addr = inet_addr(ip);  /*32 bit Internet address network byte ordered*/
+     server_addr.sin_port = htons(port);             /*server TCP port must be network byte ordered*/
 
-     /*open a TCP socket*/
      if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
            perror("socket()");
            exit(-1);
      }
-     /*connect to the server*/
+
      if (connect(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
         perror("connect()");
         exit(-1);
@@ -95,265 +66,184 @@ int createSocket(char *ip, int port) { // copied from given code
      return sockfd;
 }
 
-void readResponse(const int socket, char *responseCode) {
-     int state = 0;
-     int index = 0;
-     char c;
+int authConn(const int socket, const char *user, const char *pass) {
+    char userCommand[MAX_LENGTH];
+    char passCommand[MAX_LENGTH];
+    char answer[MAX_LENGTH];
 
-     while (state != 3) {	
-          read(socket, &c, 1);
-          printf("%c", c);
+    snprintf(userCommand, sizeof(userCommand), "user %s\n", user);
+    snprintf(passCommand, sizeof(passCommand), "pass %s\n", pass);
 
-          switch (state) {
-               case 0:
-                    if (c == ' ') {
-                         if (index != 3) {
-                              printf(" > Error in code\n");
-                              return;
-                         }
-                         index = 0;
-                         state = 1;
-                    } else {
-                         if (c == '-') {
-                              state = 2;
-                              index = 0;
-                         } else if (isdigit(c)) {
-                              responseCode[index++] = c;
-                         }
-                    }
-                    break;
-               case 1:
-                    if (c == '\n') {
-                         state = 3;
-                    }
-                    break;
-               case 2:
-                    if (c == responseCode[index]) {
-                         index++;
-                    } else if (index == 3 && c == ' ') {
-                         state = 1;
-                    } else if (index == 3 && c == '-') {
-                         index = 0;
-                    }
-                    break;
-          }
-     }
-     printf(" > Exiting readResponse function\n");
+    write(socket, userCommand, strlen(userCommand));
+    
+    if (readResponse(socket, answer) != SV_READY4PASS) {
+        printf("Unknown user '%s'. Abort.\n", user);
+        exit(EXIT_FAILURE);
+    }
+
+    write(socket, passCommand, strlen(passCommand));
+    return readResponse(socket, answer);
 }
 
+int passiveMode(const int socket, char *ip, int *port) {
+    char answer[MAX_LENGTH];
+    int ip1, ip2, ip3, ip4, port1, port2;
 
-void getFile(const int socket, char *filename) {
-     FILE *fd = fopen(filename, "wb+");
+    write(socket, "pasv\n", 5);
+    if (readResponse(socket, answer) != SV_PASSIVE) {
+        return -1;
+    }
 
-     if(fd == NULL){
-          printf(" > Error getting file\n");
-          exit(-1);
-     }
+    sscanf(answer, PASSIVE_REGEX, &ip1, &ip2, &ip3, &ip4, &port1, &port2);
+    *port = port1 * 256 + port2;
+    snprintf(ip, MAX_LENGTH, "%d.%d.%d.%d", ip1, ip2, ip3, ip4);
 
-     int bytes_num = 1;
-     char buffer[MAX_SIZE];
-
-     while(bytes_num > 0){
-          bytes_num = read(socket, buffer, MAX_SIZE);
-          if (bytes_num > 0 && fwrite(buffer, bytes_num, 1, fd) < 0) {
-               fclose(fd);
-          }
-     }
-
-     fclose(fd);
-     printf(" > Exiting getFile\n");
+    return SV_PASSIVE;
 }
 
-void parseFilename(char *path, char *filename){
-     int indexFilename = 0;
-     size_t pathLength = strlen(path);
+int readResponse(const int socket, char *buffer) {
+    char byte;
+    int index = 0, responseCode;
+    ResponseState state = START;
 
-     // reset the filename buffer
-     memset(filename, 0, 80);
+    memset(buffer, 0, MAX_LENGTH);
 
-     for (size_t indexPath = 0; indexPath < pathLength; ++indexPath){
-          if (path[indexPath] == '/'){
-            // reset when encountering /
-            indexFilename = 0;
-            memset(filename, 0, 80);
-          } 
-          else{
-            // copy characters to the filename buffer
-            filename[indexFilename++] = path[indexPath];
-          }
-     }
+    while (state != END) {
+        read(socket, &byte, 1);
 
-     printf(" > Exiting parseFilename\n");
-}
-
-struct hostent *getHostInfo(const char host[]) {
-     struct hostent *hostInfo;
-
-     // retrieve host information
-     if ((hostInfo = gethostbyname(host)) == NULL) {
-          perror("gethostbyname");
-          exit(EXIT_FAILURE);
-     }
-
-     printf(" > Exiting getHostInfo\n");
-     return hostInfo;
-}
-
-void parseArgument(char *argument, char *user, char *pass, char *host, char *path) {
-     char start[] = "ftp://";
-     int index = 0;
-     int i = 0;
-     int state = 0;
-     int length = strlen(argument);
-
-     while (i < length) {
-          switch (state) {
-          case 0: // reads the ftp://
-               if (argument[i] == start[i] && i < 5) {
+        switch (state) {
+               case START:
+                    if (byte == ' ') state = SINGLE;
+                    else if (byte == '-') state = MULTIPLE;
+                    else if (byte == '\n') state = END;
+                    else buffer[index++] = byte;
                     break;
-               }
-               if (i == 5 && argument[i] == start[i])
-                    state = 1;
-               else
-                    printf(" > Error parsing ftp://");
-               break;
-          case 1: // reads the username
-               if (argument[i] == ':') {
-                    state = 2;
+               case SINGLE:
+                    if (byte == '\n') state = END;
+                    else buffer[index++] = byte;
+                    break;
+               case MULTIPLE:
+               if (byte == '\n') {
+                    memset(buffer, 0, MAX_LENGTH);
+                    state = START;
                     index = 0;
                } else {
-                    user[index] = argument[i];
-                    index++;
+                    buffer[index++] = byte;
                }
                break;
-          case 2: // reads the password
-               if (argument[i] == '@') {
-                    state = 3;
-                    index = 0;
-               } else {
-                    pass[index] = argument[i];
-                    index++;
-               }
-               break;
-          case 3: // reads the host
-               if (argument[i] == '/') {
-                    state = 4;
-                    index = 0;
-               } else {
-                    host[index] = argument[i];
-                    index++;
-               }
-               break;
-          case 4: // reads the path
-               path[index] = argument[i];
-               index++;
-               break;
-          }
-
-          i++;
-     }
-     printf(" > Exiting parseArgument\n");
-}
-
-
-int getServerPortFromResponse(int socketfd) {
-     int state = 0;
-     int index = 0;
-     char firstByte[4] = {0};  // Initialize with zeros
-     char secondByte[4] = {0}; // Initialize with zeros
-     char current;
-
-     while (state != 7) {
-          read(socketfd, &current, 1);
-          printf("%c", current);
-
-          switch (state) {
-          case 0:
-               // Waits for 3 digit number followed by ' '
-               if (current == ' ') {
-                    if (index != 3) {
-                         printf(" > Error receiving response code\n");
-                         return -1;
-                    }
-                    index = 0;
-                    state = 1;
-               } else {
-                    index++;
-               }
-               break;
-          case 5:
-               // Reads until the first comma, extracting the first byte of the port
-               if (current == ',') {
-                    index = 0;
-                    state++;
-               } else {
-                    firstByte[index++] = current;
-               }
-               break;
-          case 6:
-               // Reads until the closing parenthesis, extracting the second byte of the port
-               if (current == ')') {
-                    state++;
-               } else {
-                    secondByte[index++] = current;
-               }
-               break;
-          default:
-               // Reads until the first comma
-               if (current == ',') {
-                    state++;
-               }
-               break;
-          }
-     }
-
-     // Converts the extracted bytes to integers and returns the calculated port
-     int firstByteInt = atoi(firstByte);
-     int secondByteInt = atoi(secondByte);
-     return (firstByteInt * 256 + secondByteInt);
-}
-
-
-int sendCommandInterpretResponse(int socketfd, char cmd[], char commandContent[], char* filename, int socketfdClient) {
-     char responseCode[3];
-     int action = 0;
-
-     // sends the command
-     write(socketfd, cmd, strlen(cmd));
-     write(socketfd, commandContent, strlen(commandContent));
-     write(socketfd, "\n", 1);
-
-     while (1) {
-          // reads response
-          readResponse(socketfd, responseCode);
-          action = responseCode[0] - '0';
-
-          switch (action) {
-          case 1:
-               // wait for response
-               if (strcmp(cmd, "retr ") == 0) {
-                    getFile(socketfdClient, filename);
+               case END:
                     break;
-               }
-               readResponse(socketfd, responseCode);
-               break;
-          case 2:
-               // command accepted
-               return 0;
-          case 3:
-               // needs more info
-               return 1;
-          case 4:
-               // try again
-               write(socketfd, cmd, strlen(cmd));
-               write(socketfd, commandContent, strlen(commandContent));
-               write(socketfd, "\r\n", 2);
-               break;
-          case 5:
-               // command rejected, exit
-               printf(" > Command rejected\n");
-               close(socketfd);
-               exit(-1);
-          }
-     }
+               default:
+                    break;
+        }
+    }
+
+    sscanf(buffer, RESPCODE_REGEX, &responseCode);
+    return responseCode;
+}
+
+
+
+int requestResource(const int socket, char *resource) {
+    char fileCommand[5 + strlen(resource) + 1], answer[MAX_LENGTH];
+
+    sprintf(fileCommand, "retr %s\n", resource);
+
+    write(socket, fileCommand, strlen(fileCommand));
+
+    return readResponse(socket, answer);
+}
+
+int getResource(const int socketA, const int socketB, char *filename) {
+    FILE *fd = fopen(filename, "wb");
+    if (fd == NULL) {
+        printf("Error opening or creating file '%s'\n", filename);
+        exit(-1);
+    }
+
+    char buffer[MAX_LENGTH];
+    int bytes;
+
+    do {
+        bytes = read(socketB, buffer, MAX_LENGTH);
+        if (bytes > 0 && fwrite(buffer, bytes, 1, fd) < 0) {
+            fclose(fd);
+            return -1;
+        }
+    } while (bytes);
+
+    fclose(fd);
+
+    return readResponse(socketA, buffer);
+}
+
+int closeConnection(const int socketA, const int socketB) {
+    char answer[MAX_LENGTH];
+
+    write(socketA, "quit\n", 5);
+    if (readResponse(socketA, answer) != SV_GOODBYE) {
+        return -1;
+    }
+
+    return close(socketA) || close(socketB);
+}
+
+
+int main(int argc, char *argv[]) {
+
+    if (argc != 2) {
+        printf("Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
+        exit(-1);
+    } 
+
+    struct URL url;
+    memset(&url, 0, sizeof(url));
+    if (parse(argv[1], &url) != 0) {
+        printf("Parse error. Usage: ./download ftp://[<user>:<password>@]<host>/<url-path>\n");
+        exit(-1);
+    }
+    
+    printf("Host: %s\nResource: %s\nFile: %s\nUser: %s\nPassword: %s\nIP Address: %s\n", url.host, url.resource, url.file, url.user, url.password, url.ip);
+
+    char answer[MAX_LENGTH];
+    int socketA = createSocket(url.ip, SERVER_PORT);
+    if (socketA < 0 || readResponse(socketA, answer) != SV_READY4AUTH) {
+        printf("Socket to '%s' and port %d failed\n", url.ip, SERVER_PORT);
+        exit(-1);
+    }
+    
+    if (authConn(socketA, url.user, url.password) != SV_LOGINSUCCESS) {
+        printf("Authentication failed with username = '%s' and password = '%s'.\n", url.user, url.password);
+        exit(-1);
+    }
+    
+    int port;
+    char ip[MAX_LENGTH];
+    if (passiveMode(socketA, ip, &port) != SV_PASSIVE) {
+        printf("Passive mode failed\n");
+        exit(-1);
+    }
+
+    int socketB = createSocket(ip, port);
+    if (socketB < 0) {
+        printf("Socket to '%s:%d' failed\n", ip, port);
+        exit(-1);
+    }
+
+    if (requestResource(socketA, url.resource) != SV_READY4TRANSFER) {
+        printf("Unknown resouce '%s' in '%s:%d'\n", url.resource, ip, port);
+        exit(-1);
+    }
+
+    if (getResource(socketA, socketB, url.file) != SV_TRANSFER_COMPLETE) {
+        printf("Error transfering file '%s' from '%s:%d'\n", url.file, ip, port);
+        exit(-1);
+    }
+
+    if (closeConnection(socketA, socketB) != 0) {
+        printf("Sockets close error\n");
+        exit(-1);
+    }
+
+    return 0;
 }
